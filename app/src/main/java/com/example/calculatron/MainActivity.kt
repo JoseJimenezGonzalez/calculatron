38cb01c1e5022742f457849cf17f398cb67b3214
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-12-01 20:02:02 +0100

    commit ok

--- app/src/main/java/com/example/calculatron/MainActivity.kt
+++ app/src/main/java/com/example/calculatron/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.calculatron
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import com.example.calculatron.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var tiempoRestante = 20000
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        object : CountDownTimer(tiempoRestante.toLong(), 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                binding.tvTiempoRestante.setText("Tiempo restante: " + millisUntilFinished / 1000)
+            }
+
+            override fun onFinish() {
+                binding.tvTiempoRestante.setText("Se ha agotado el tiempo!")
+            }
+        }.start()
     }
 }
