83e7c851cc4f88f76d57cb19a45d91b398f0d2f8
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-12-01 20:35:54 +0100

    commit ok

--- app/src/main/java/com/example/calculatron/MainActivity.kt
+++ app/src/main/java/com/example/calculatron/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.calculatron
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -9,9 +13,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var tiempoRestante = 20000
+    private var tiempoRestante = 0 // Inicializar con 0 para evitar nullPointerException
+    private lateinit var sharedPreferences: SharedPreferences
 
 
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,15 +25,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        sharedPreferences = getSharedPreferences("nombrePref", Context.MODE_PRIVATE)
+
+        // Obtener el tiempo predeterminado guardado en SharedPreferences
+        tiempoRestante = sharedPreferences.getInt(getString(R.integer.tiempo_predeterminado), 20000)
+
         object : CountDownTimer(tiempoRestante.toLong(), 1000) {
 
             override fun onTick(millisUntilFinished: Long) {
-                binding.tvTiempoRestante.setText("Tiempo restante: " + millisUntilFinished / 1000)
+                binding.tvTiempoRestante.text = "Tiempo restante: " + millisUntilFinished / 1000
             }
 
             override fun onFinish() {
-                binding.tvTiempoRestante.setText("Se ha agotado el tiempo!")
+                binding.tvTiempoRestante.text = "Se ha agotado el tiempo!"
             }
         }.start()
+
+        binding.ivOpciones.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/calculatron/MainActivity2.kt
+++ app/src/main/java/com/example/calculatron/MainActivity2.kt
@@ -1,11 +1,34 @@
 package com.example.calculatron
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
+import com.example.calculatron.databinding.ActivityMain2Binding
+import com.example.calculatron.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtener una referencia a SharedPreferences
+        val sharedPreferences: SharedPreferences = getSharedPreferences("nombrePref", Context.MODE_PRIVATE)
+
+        binding.btnGuardarConfiguracion.setOnClickListener {
+            // Modificar el tiempo predeterminado
+            val nuevoTiempoPredeterminado = binding.tietCuentaAtras.text.toString().toInt()
+            val claveTiempoPredeterminado = getString(R.integer.tiempo_predeterminado)
+            sharedPreferences.edit {
+                putInt(claveTiempoPredeterminado, nuevoTiempoPredeterminado)
+            }
+        }
     }
 }
