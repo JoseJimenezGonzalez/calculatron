52a1a95c5466a7461e74a8d909b8fea811f47243
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-12-16 23:07:10 +0100

    commit ok

--- app/src/main/java/com/example/calculatron/MainActivity.kt
+++ app/src/main/java/com/example/calculatron/MainActivity.kt
@@ -27,15 +27,13 @@ class MainActivity : AppCompatActivity() {
     private var acertadas = 0
     private var falladas = 0
 
-    //Maximo y minimo
-    var maximo = 10 // Inicializo a 10 para evitar excepcion
-    var minimo = 0 // Inicializo a 0 para evitar excepcion
-
     // Nuevas variables para almacenar cuentas anteriores y siguientes
     private var operacionActual = ""
-    private var operandosActuales = arrayOf(0, 0)
+    var operandoActual1 = 0
+    var operandoActual2 = 0
 
     private val nombrePref = "mis_preferencias"
+    private lateinit var sharedPreferences: SharedPreferences
 
 
     @SuppressLint("ResourceType")
@@ -46,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val sharedPreferences = getSharedPreferences(nombrePref, Context.MODE_PRIVATE)
+        sharedPreferences = getSharedPreferences(nombrePref, Context.MODE_PRIVATE)
 
         //Teclado y entrada de datos de usuario
         val edtInput: EditText = findViewById(R.id.edtEntradaUsuario)
@@ -76,13 +74,6 @@ class MainActivity : AppCompatActivity() {
             }
         }.start()
 
-
-        //Obtener el maximo predeterminado guardado en el sharedpreferences
-        maximo = sharedPreferences.getInt("valor_maximo_predeterminado", 10)
-
-        //Obtener el minimo predeterminado guardado en el sharedpreferences
-        minimo = sharedPreferences.getInt("valor_minimo_predeterminado", 0)
-
         binding.ivOpciones.setOnClickListener {
             val intent = Intent(this@MainActivity, MainActivity2::class.java)
             startActivity(intent)
@@ -91,33 +82,40 @@ class MainActivity : AppCompatActivity() {
         //Meter codigo
 
 
-        binding.tvCuentaActual.text = generarCuentaString()
+        generarOperacion()
+        operandoActual1 = generarOperandos()
+        operandoActual2 = generarOperandos()
         Log.e("operacion actual", "$operacionActual")
-        Log.e("operandos actuales", "op1${operandosActuales[0]}, op2${operandosActuales[1]}")
+        Log.e("operandos actuales", "op1$operandoActual1, op2$operandoActual2")
+
+        binding.tvPrimerOperando.text = operandoActual1.toString()
+        binding.tvSegundoOperando.text = operandoActual2.toString()
+        binding.tvOperacion.text = operacionActual
     }
-    fun generarOperacion(): String {
+    fun generarOperacion(){
         val operacionesPermitidas = getResources().getStringArray(R.array.operaciones_permitidas)
         val randomOperationIndex = random.nextInt(operacionesPermitidas.size)
         Log.e("generarOperacion()", "Return: ${operacionesPermitidas[randomOperationIndex]}")
         operacionActual = operacionesPermitidas[randomOperationIndex]
-        return operacionesPermitidas[randomOperationIndex]
     }
 
-    fun generarOperandos(): Array<Int>{
-        //Me genera los numeros que estan entre el minimo y el maximo
-        var operandoActual1 = random.nextInt(maximo - minimo + 1) + minimo
-        var operandoActual2 = random.nextInt(maximo - minimo + 1) + minimo
-        Log.e("generarOperandos()", "Return: op1$operandoActual1, op2$operandoActual2")
-        operandosActuales = arrayOf(operandoActual1, operandoActual2)
-        return arrayOf(operandoActual1, operandoActual2)
+    fun generarOperandos(): Int {
+        //Obtener el maximo predeterminado guardado en el sharedpreferences
+        var maximo = sharedPreferences.getInt("valor_maximo_predeterminado", 10)
+
+        //Obtener el minimo predeterminado guardado en el sharedpreferences
+        var minimo = sharedPreferences.getInt("valor_minimo_predeterminado", 0)
+        //Me genera los numeros que están entre el minimo y el maximo
+        return random.nextInt(maximo - minimo + 1) + minimo
     }
+
 
 
 
     fun comprobarResultado(respuestaUsuario: Int) {
         when (operacionActual) {
             "+" -> {
-                val calculoResultado = operandosActuales[0] + operandosActuales[1]
+                val calculoResultado = operandoActual1 + operandoActual2
                 if(respuestaUsuario == calculoResultado){
                     acertadas++
                     binding.tvAcertadas.text = "Acertadas: $acertadas"
@@ -127,7 +125,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             "-" -> {
-                val calculoResultado = operandosActuales[0] - operandosActuales[1]
+                val calculoResultado = operandoActual1 - operandoActual2
                 if(respuestaUsuario == calculoResultado){
                     acertadas++
                     binding.tvAcertadas.text = "Acertadas: $acertadas"
@@ -137,7 +135,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             "*" -> {
-                val calculoResultado = operandosActuales[0] * operandosActuales[1]
+                val calculoResultado = operandoActual1 * operandoActual2
                 if(respuestaUsuario == calculoResultado){
                     acertadas++
                     binding.tvAcertadas.text = "Acertadas: $acertadas"
@@ -147,13 +145,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-    }
-
-    private fun generarCuentaString(): String {
-        val operacion = generarOperacion()
-        val operandos = generarOperandos()
-        Log.e("generarCuentaString()", "Return: op$operacion, op1${operandos[0]}, op2${operandos[1]}")
-        return "${operandos[0]} $operacion ${operandos[1]} = "
     }
 
 
